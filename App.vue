4b64d181ebd05d241fcb3cf49f7c3e328a1b4b5c
ylxcqzp <2296253727@qq.com>
2023-04-27 09:56:33 +0800

    新建项目
    Signed-off-by: ylxcqzp <2296253727@qq.com>

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	
+	/*引入uview基础样式*/
+	@import '@/uni_modules/uview-ui/index.scss';
+	
+	/*引入icon图标库*/
+	@import url("static/icon/iconfont.css");
+</style>
